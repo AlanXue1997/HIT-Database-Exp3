cfceaa53614025dc1fa464430a2ff8c1ea9599b5
AlanXue1997 <836106158@qq.com>
2018-05-13 19:44:11 +0800

    完成：能够编辑、删除日志

--- exp3/urls.py
+++ exp3/urls.py
@@ -6,13 +6,26 @@ app_name = 'exp3'
 urlpatterns = [
     path('', views.login, name='login'),
     path('<int:pk>/', views.IndexView.as_view(), name='index'),
+
+    # Info
     path('<int:user_id>/changeInfo/', views.changeInfo, name='changeInfo'),
+
+    # Education
     path('<int:pk>/education', views.EducationView.as_view(), name='education'),
     path('<int:education_id>/changeEducationInfo/', views.changeEducationInfo, name='changeEducationInfo'),
     path('<int:education_id>/deleteEducationInfo/', views.deleteEducationInfo, name='deleteEducationInfo'),
     path('<int:user_id>/newEducationInfo/', views.newEducationInfo, name='newEducationInfo'),
+
+    # Work
     path('<int:pk>/work', views.WorkView.as_view(), name='work'),
     path('<int:work_id>/changeWorkInfo/', views.changeWorkInfo, name='changeWorkInfo'),
     path('<int:work_id>/deleteWorkInfo/', views.deleteWorkInfo, name='deleteWorkInfo'),
     path('<int:user_id>/newWorkInfo/', views.newWorkInfo, name='newWorkInfo'),
+
+    # Diary
+    path('<int:pk>/diaryList', views.DiarysListView.as_view(), name='diaryList'),
+    path('<int:user_id>/diary/<int:diary_id>/', views.diarypage, name='diary'),
+    path('<int:user_id>/editDiary/<int:diary_id>/', views.editDiary, name='editDiary'),
+    path('<int:user_id>/editDiary/<int:diary_id>/push', views.pushEditDiary, name='pushEditDiary'),
+    path('<int:user_id>/editDiary/<int:diary_id>/delete', views.deleteEditDiary, name='deleteEditDiary'),
 ]
--- exp3/views.py
+++ exp3/views.py
@@ -5,93 +5,21 @@ from django.http import HttpResponseRedirect
 from django import forms
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
-from .models import User, Education, Work
+from .models import User, Education, Work, Diary
+
+# ------------------------------
+# Login
+# ------------------------------
 
 class IndexView(generic.DetailView):
     model = User
     template_name = 'exp3/index.html'
 
-class EducationView(generic.DetailView):
-    model = User
-    template_name = 'exp3/education.html'
-
-class WorkView(generic.DetailView):
-    model = User
-    template_name = 'exp3/work.html'
-
 class UserForm(forms.Form):
     email = forms.CharField(label='邮箱')
     password = forms.CharField(label='密码',widget=forms.PasswordInput())
-
-def newWorkInfo(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    user.work_set.create(
-        company=request.POST['company'],
-        position=request.POST['position'],
-        begin=request.POST['begin'],
-        end=request.POST['end']
-    )
-    return HttpResponseRedirect(reverse('exp3:work', args=(user.id,)))
-
-def deleteWorkInfo(request, work_id):
-    work = get_object_or_404(Work, pk=work_id)
-    work.delete()
-    return HttpResponseRedirect(reverse('exp3:work', args=(work.user.id,)))
-
-def changeWorkInfo(request, work_id):
-    work = get_object_or_404(Work, pk=work_id)
-    try:
-        work.company = request.POST['company']
-        work.position = request.POST['position']
-        work.begin = request.POST['begin']
-        work.end = request.POST['end']
-        work.save()
-    except:
-        return HttpResponseRedirect(reverse('exp3:work', args=(work.user.id,)))
-    else:
-        return HttpResponseRedirect(reverse('exp3:work', args=(work.user.id,)))
-
-def newEducationInfo(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    user.education_set.create(
-        school=request.POST['school'],
-        level=request.POST['level'],
-        begin=request.POST['begin'],
-        end=request.POST['end']
-    )
-    return HttpResponseRedirect(reverse('exp3:education', args=(user.id,)))
-
-def deleteEducationInfo(request, education_id):
-    education = get_object_or_404(Education, pk=education_id)
-    education.delete()
-    return HttpResponseRedirect(reverse('exp3:education', args=(education.user.id,)))
-
-def changeEducationInfo(request, education_id):
-    education = get_object_or_404(Education, pk=education_id)
-    try:
-        education.school = request.POST['school']
-        education.level = request.POST['level']
-        education.begin = request.POST['begin']
-        education.end = request.POST['end']
-        education.save()
-    except:
-        return HttpResponseRedirect(reverse('exp3:education', args=(education.user.id,)))
-    else:
-        return HttpResponseRedirect(reverse('exp3:education', args=(education.user.id,)))
-
-def changeInfo(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    try:
-        user.name = request.POST['name']
-        user.sex = request.POST['sex']
-        user.birth = request.POST['birth']
-        user.address = request.POST['address']
-        user.save()
-    except:
-        return HttpResponseRedirect(reverse('exp3:index', args=(user.id,)))
-    else:
-        return HttpResponseRedirect(reverse('exp3:index', args=(user.id,)))
 
 def login(request):
     if request.method == 'POST':
@@ -122,3 +50,129 @@ def login(request):
 
     uf = UserForm()
     return render(request, 'exp3/login.html', {'uf': uf})
+
+# ------------------------------
+# About work
+# ------------------------------
+
+class WorkView(generic.DetailView):
+    model = User
+    template_name = 'exp3/work.html'
+
+def newWorkInfo(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.work_set.create(
+        company=request.POST['company'],
+        position=request.POST['position'],
+        begin=request.POST['begin'],
+        end=request.POST['end']
+    )
+    return HttpResponseRedirect(reverse('exp3:work', args=(user.id,)))
+
+def deleteWorkInfo(request, work_id):
+    work = get_object_or_404(Work, pk=work_id)
+    work.delete()
+    return HttpResponseRedirect(reverse('exp3:work', args=(work.user.id,)))
+
+def changeWorkInfo(request, work_id):
+    work = get_object_or_404(Work, pk=work_id)
+    try:
+        work.company = request.POST['company']
+        work.position = request.POST['position']
+        work.begin = request.POST['begin']
+        work.end = request.POST['end']
+        work.save()
+    except:
+        return HttpResponseRedirect(reverse('exp3:work', args=(work.user.id,)))
+    else:
+        return HttpResponseRedirect(reverse('exp3:work', args=(work.user.id,)))
+
+
+# ------------------------------
+# About Education
+# ------------------------------
+
+class EducationView(generic.DetailView):
+    model = User
+    template_name = 'exp3/education.html'
+
+def newEducationInfo(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    user.education_set.create(
+        school=request.POST['school'],
+        level=request.POST['level'],
+        begin=request.POST['begin'],
+        end=request.POST['end']
+    )
+    return HttpResponseRedirect(reverse('exp3:education', args=(user.id,)))
+
+def deleteEducationInfo(request, education_id):
+    education = get_object_or_404(Education, pk=education_id)
+    education.delete()
+    return HttpResponseRedirect(reverse('exp3:education', args=(education.user.id,)))
+
+def changeEducationInfo(request, education_id):
+    education = get_object_or_404(Education, pk=education_id)
+    try:
+        education.school = request.POST['school']
+        education.level = request.POST['level']
+        education.begin = request.POST['begin']
+        education.end = request.POST['end']
+        education.save()
+    except:
+        return HttpResponseRedirect(reverse('exp3:education', args=(education.user.id,)))
+    else:
+        return HttpResponseRedirect(reverse('exp3:education', args=(education.user.id,)))
+
+# ------------------------------
+# About Info
+# ------------------------------
+
+def changeInfo(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    try:
+        user.name = request.POST['name']
+        user.sex = request.POST['sex']
+        user.birth = request.POST['birth']
+        user.address = request.POST['address']
+        user.save()
+    except:
+        return HttpResponseRedirect(reverse('exp3:index', args=(user.id,)))
+    else:
+        return HttpResponseRedirect(reverse('exp3:index', args=(user.id,)))
+
+# ------------------------------
+# About diary
+# ------------------------------
+
+class DiarysListView(generic.DetailView):
+    model = User
+    template_name = 'exp3/diaryList.html'
+
+def diarypage(request, user_id, diary_id):
+    diary = get_object_or_404(Diary, pk=diary_id)
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'exp3/diary.html', {'diary': diary, 'user' : user })
+
+def editDiary(request, user_id, diary_id):
+    diary = get_object_or_404(Diary, pk=diary_id)
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'exp3/editDiary.html', {'diary': diary, 'user': user})
+
+def pushEditDiary(request, user_id, diary_id):
+    diary = get_object_or_404(Diary, pk=diary_id)
+    try:
+        diary.author = User.objects.get(pk=user_id)
+        diary.title = request.POST['title']
+        diary.content = request.POST['content']
+        diary.date = timezone.now()
+        diary.save()
+    except:
+        return HttpResponseRedirect(reverse('exp3:diary', args=(user_id, diary_id,)))
+    else:
+        return HttpResponseRedirect(reverse('exp3:diary', args=(user_id, diary_id,)))
+
+def deleteEditDiary(request, user_id, diary_id):
+    diary = get_object_or_404(Diary, pk=diary_id)
+    diary.delete()
+    return HttpResponseRedirect(reverse('exp3:diaryList', args=(user_id,)))
